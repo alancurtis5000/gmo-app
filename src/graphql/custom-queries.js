7cc855487840785193236737863c0f136d9e2259
Alan <alancurtis5000+accounts@gmail.com>
2021-07-18 12:56:08 -0600

    missed custom-queries.js

---
+++ src/graphql/custom-queries.js
@@ -0,0 +1,11 @@
+// export const getUserByUserSub = /* GraphQL */ `
+//   query GetUserByUserSub($userSub: ID!) {
+//     getUser(userSub: $userSub) {
+//       id
+//       userSub
+//       name
+//       createdAt
+//       updatedAt
+//     }
+//   }
+// `;
