bdf80aad351f8996ab7be5b0b228085f0e3b7eb8
Alan <alancurtis5000+accounts@gmail.com>
2021-07-25 17:59:14 -0600

    removed create character  into  user character

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -3,7 +3,6 @@
 import { combineReducers } from "redux";
 
 // import exampleCounterReducer from "./example-counter/example-counter.reducer";
-import createCharacterReducer from "./create-character/create-character.reducer";
 import gameReducer from "./game/game.reducer";
 import userReducer from "./user/user.reducer";
 import userCharacterReducer from "./user-character/user-character.reducer";
@@ -11,7 +10,6 @@ import userCharactersReducer from "./user-characters/user-characters.reducer";
 
 export default combineReducers({
   //   exampleCounter: exampleCounterReducer,
-  createCharacter: createCharacterReducer,
   game: gameReducer,
   user: userReducer,
   userCharacter: userCharacterReducer,
