c4d79f285a53a64bb5048692b20ada056b2b897b
Alan <alancurtis5000+accounts@gmail.com>
2021-08-03 22:31:23 -0600

    added progressive web app capablities

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import config from "./aws-exports";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "./redux/configure-store";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 Amplify.configure(config);
 const store = configureStore();
@@ -27,3 +28,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
