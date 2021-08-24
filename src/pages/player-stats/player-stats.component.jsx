763ea72b1b81b05e889e7876f52d2bc34f2be16e
Alan <alancurtis5000+accounts@gmail.com>
2021-08-23 23:14:17 -0600

    removed old player-stats-card

--- src/pages/player-stats/player-stats.component.jsx
+++ src/pages/player-stats/player-stats.component.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import CharacterDataCard from "../../components/character-data-card/character-data-card.component";
-import PlayerStatsCard from "../../components/player-stats-card/player-stats-card.component";
 
 const PlayerStatsPage = () => {
   const character = useSelector((state) => state.userCharacter.data);
